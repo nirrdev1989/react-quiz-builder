dd97d4b8e56025b3fb9313c296d0d8ded1c9dde6
ruslanNir <nirkuba19999@gmail.com>
2021-06-01 03:31:46 +0300

    not found page

---
+++ src/pages/NotPound.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotPoundPage() {
+   return (
+      <div style={{ textAlign: 'center', marginTop: '2rem', fontSize: '18px', fontWeight: 600 }}>
+         Not Found
+      </div>
+   )
+}
+
+export default NotPoundPage
